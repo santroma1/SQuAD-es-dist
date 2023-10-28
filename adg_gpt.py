13e355913dfb5e2d8af766c21c9af19e5793894a
Santiago Roman <santroma1@gmail.com>
2023-10-28 11:03:12 +0200

    Final edit to GPT generated distractors

--- adg_gpt.py
+++ adg_gpt.py
@@ -7,6 +7,7 @@ import dotenv
 import os
 import jsonlines as jsl
 import time
+import sys
 
 
 SEP = "[SEP]"
@@ -17,7 +18,7 @@ if __name__ == '__main__':
 
     openai.api_key = os.getenv('DG_GPT_KEY')
 
-    default_prompt = "La generación de distractores en el contexto de preguntas de opción múltiple es una tarea que con un texto, una pregunta, y una respuesta correcta, se busca generar las respuestas distractoras. Las respuestas distractoras son respuestas incorrectas a una pregunta, pero que tienen la capacidad de confundir a un estudiante, y que son sintácticamente similares a la respuesta correcta. Las respuestas distractoras deben ser gramáticamente similares a la respuesta correcta, por ejemplo, si la respuesta correcta empieza con una letra minúscula, las respuestas distractoras también deben empezar con una letra minúscula. Las respuestas distractoras serán mejores también si pertenecen al mismo grupo semántico que la respuesta correcta. Voy a poner un texto, una pregunta, y la respuesta correcta, y tu tarea es generar 3 respuestas distractoras. Recuerda que debes generar buenas respuestas distractoras como las describí. Es muy importante que las respuestas distractoras deben ser extraídas del texto, y que las respuestas distractoras no sean explícitamente la respuesta correcta. Si haces las respuestas distractoras similares entre ellas, pero diferentes a la respuesta correcta, un estudiante podría intuir cuál es la respuesta correcta, al ver que hay una respuesta que es diferente en su forma a las demás."
+    default_prompt = "Eres un maestro para estudiantes que están aprendiendo español. Te asignan la tarea de generar distractores para preguntas de opción múltiple, con el propósito de evaluar a tus estudiantes. La generación de distractores en el contexto de preguntas de opción múltiple es una tarea que con un texto, una pregunta, y una respuesta correcta, se busca generar las respuestas distractoras. Las respuestas distractoras son respuestas incorrectas a una pregunta, pero que tienen la capacidad de confundir a un estudiante, y que son sintácticamente similares a la respuesta correcta. Las respuestas distractoras deben ser gramáticamente similares a la respuesta correcta, por ejemplo, si la respuesta correcta empieza con una letra minúscula, las respuestas distractoras también deben empezar con una letra minúscula. Las respuestas distractoras serán mejores también si pertenecen al mismo grupo semántico que la respuesta correcta. Serás presentado con un texto, una pregunta, y la respuesta correcta, y tu tarea es generar 3 respuestas distractoras. Recuerda que debes generar buenas respuestas distractoras como las describí. Primero lee el texto. Segundo, lee la pregunta y la respuesta correcta. Y tercero, con base en el texto, la pregunta, y la respuesta correcta, extrae del texto las 3 respuestas distractoras. Las respuestas distractoras deben ser extraídas del texto, y las respuestas distractoras no deben ser explícitamente la respuesta correcta. Si haces las respuestas distractoras similares entre ellas, pero diferentes a la respuesta correcta, un estudiante podría intuir cuál es la respuesta correcta, al ver que hay una respuesta que es diferente en su forma a las demás. Genera las respuestas distractoras en un formato de lista de Python, y cada una debe empezar con letras minúsculas."
 
     # current_file = "dev.json"
     current_file = "test.json"
@@ -36,35 +37,33 @@ if __name__ == '__main__':
             question = record["question"]
             choices = record["choices"]
             ans = None
-            dists = []
+            real_dists = []
 
             for choice in choices:
                 if choice["type"]=="correct answer":
                     ans = choice["text"]
                 else:
-                    dists.append(choice["text"])
-
-            if (current_file == "test.json") and (len(dists) < 3):
-                continue
+                    real_dists.append(choice["text"])
+            
 
             while True:
                 try:
-                    final_prompt = f'{default_prompt}\n\nTexto: {text}\n\nPregunta: {question}\n\nRespuesta Correcta: {ans}'
+                    final_prompt = f'{default_prompt}\n\nTexto: {text}\n\nPregunta: {question}\n\nRespuesta Correcta: {ans}\n\n Respuestas Distractoras:\n'
                     # print(final_prompt)
                     gen_params = {
-                        'prompt': final_prompt,
+                        "messages":[{"role": "user","content": final_prompt}],
                         'temperature': temp,
                         'max_tokens': 1024
                     }
-                    completion = openai.Completion.create(engine='text-davinci-003', **gen_params)
+                    completion = openai.ChatCompletion.create(model='gpt-4', **gen_params)
                     
                 except openai.error.RateLimitError:
                     time.sleep(60)
                     continue
 
-                generated_distractors = completion["choices"][0]['text']
+                generated_distractors = completion["choices"][0]['message']['content']
                 context_str = f"{CLS} {text} {SEP} {question} {SEP} {ans} {SEP}"
-                writer.write(f"{context_str}{generated_distractors}\n{dists}\n\n")
+                writer.write(f"{context_str}\n{generated_distractors}\n{real_dists}\n\n")
 
                 break
     
